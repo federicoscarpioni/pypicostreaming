61fff89e73ef6365c2b081fbc741ad4999c65820
Federico Scarpioni <federicoscarpioni@gmail.com>
2024-09-02 11:13:02 +0200

    Updated test

--- tests/series5000_test.py
+++ tests/series5000_test.py
@@ -5,19 +5,19 @@ This is a test for the Picoscope class.
 from pypicostreaming.series5000.series5000 import Picoscope5000a
 
 # Measurment paramters
-capture_size = 20
-samples_total = 1000
-sampling_time = 100
-sampling_time_scale = 'PS5000A_MS'
+capture_size = 10000
+samples_total = 5000000
+sampling_time = 20
+sampling_time_scale = 'PS5000A_US'
 
 # Connect i nstrument and perform the acquisiton
 pico = Picoscope5000a('PS5000A_DR_14BIT')
-saving_path = 'E:/Experimental_data/Federico/2024/python_software_test/2408281137_test_CP_full_dummy_3electrodes_pico_connected_sine_wave_smaller_IRange'
+saving_path = 'E:/Experimental_data/Federico/2024/LRE-Reference_electrode_for_lithium/PCLRE011_3e/2408301238_PCLRE011_CP_100uA_10min_DEIS_3e_corrected_pico'
 pico.set_pico(capture_size, samples_total, sampling_time, sampling_time_scale, saving_path)
-pico.set_channel('PS5000A_CHANNEL_A', 'PS5000A_200MV')
-pico.set_channel('PS5000A_CHANNEL_B', 'PS5000A_200MV', 0.0001)
+pico.set_channel('PS5000A_CHANNEL_A', 'PS5000A_500MV')
+pico.set_channel('PS5000A_CHANNEL_B', 'PS5000A_2V', 0.01)
 pico.set_channel('PS5000A_CHANNEL_C', 'PS5000A_500MV')
-pico.run_streaming_non_blocking(autoStop= True)
+pico.run_streaming_non_blocking(autoStop= False)
 
 #%%
 pico.stop()
